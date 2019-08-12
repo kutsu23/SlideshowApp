3cbd93fc875d1ded02ae21e658874b97d7583d94
kutsu23 <kutsu23@gmail.com>
2019-08-12 10:02:40 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 沓掛史典 on 2019/08/12.
+//  Copyright © 2019 fuminori.kutsukake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
